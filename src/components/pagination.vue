a5dc348cdae922da83db31cf15e271f6174f3cbc
zzzkun <39958482+zzzkun@users.noreply.github.com>
2019-04-19 17:35:42 +0800

    Add files via upload

--- src/components/pagination.vue
+++ src/components/pagination.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="pagination">
-     <button @click="change">首页</button>
-     <button  @click="change">上一页</button>
+  <div class="pagination" @click="change($event)">
+     <button>首页</button>
+     <button  >上一页</button>
      <button  v-if="jduge" class="pagebtn">...</button>
-     <button v-for="(item, index) in items" :key="index" :class="[{Page:item == current},'pagebtn']"  @click="change(item)">{{item}}</button>
-     <button  @click="change">下一页</button>
+     <button v-for="(item, index) in items" :key="index" :class="[{Page:item == current},'pagebtn']">{{item}}</button>
+     <button  >下一页</button>
   </div>
 </template>
 
 <script>
-import $ from 'jquery'
 export default {
   components:{},
   props:{
     isRest:{
       type:Boolean,
-      // default:false
+      default:false
     
     }
   },
@@ -35,46 +34,45 @@ export default {
   },
   computed:{},
   methods:{
-    change(value){
-      if (typeof value != 'number') {
-        switch (value.target.innerText) {
+    change($event) {
+      // console.log($event.target);
+      let value = $event.target
+      if (value.className == 'pagination') return;
+      if ((value.innerText - 0) != value.innerText) { // NaN == NaN => false
+        switch (value.innerText) {
           case '上一页':
-
-            $('button.Page').prev().click();
-
+            if (this.current > 1) {
+              this.current--
+            }
             break;
-         case '下一页':
-
-            $('button.Page').next().click();
+          case '下一页':
+            this.current++
             break;
-            case '首页':
-            
-            this.items = [1,2,3,4,5,'.....'];
-            this.change(1);
-            
-          default:
-
+          case '首页':
+            this.items = [1, 2, 3, 4, 5, '.....'];
+            this.current = 1
             break;
         }
-        return
-      } 
-       this.current=value
-       if (value>4) {
-         this.jduge = true
-       } else {
+      } else {
+        // console.log(value.innerText)
+        if (value.innerText > 4) {
+          this.jduge = true
+        } else {
           this.jduge = false
-       }
-       if (value ==this.items[4]) {
-         this.items.shift()//移除第一个元素
-         this.items.splice(4,0,this.items[3]+1)   //splice()方法删除位于 index 4 的元素，并添加一个新元素来替代被删除的元素。
-       } else if(value ==this.items[0]&& value != 1 ) {
-         this.items.unshift(this.items[0]-1)
-         this.items.splice(5,1)
-       }
-      //  console.log(value)
-      // debugger
-       this.$emit('handle', this.current); //子传父组件的方法  触发自定义handle的函数  其中handle为父组件定义函数，this.current为需要传递参数
-    },
+        }
+        this.current = Number(value.innerText)
+      }
+      if (this.current == this.items[4]) {
+        this.items.shift() //移除第一个元素
+        this.items.splice(4, 0, this.items[3] + 1) //splice()方法删除位于 index 4 的元素，并添加一个新元素来替代被删除的元素。
+      } else if (this.current == this.items[0] && this.current > 1) {
+        this.items.unshift(this.items[0] - 1)
+        this.items.splice(5, 1)
+      }
+
+      this.$emit('handle', this.current); //子传父组件的方法  触发自定义handle的函数  其中handle为父组件定义函数，this.current为需要传递参数
+
+    }
   },
   created(){},
   mounted(){}
